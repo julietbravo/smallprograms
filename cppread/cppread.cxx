d6906d9dbd98cc346e556fc6b30857bd907d4138
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-01-18 13:25:27 +0100

    Enable exceptions in case of illegal file name or corrupted file

--- cppread/cppread.cxx
+++ cppread/cppread.cxx
@@ -15,182 +15,191 @@ void checkItem(const T &t) {}
 template<>
 void checkItem(const std::string &s)
 {
-  // Check whether string is empty or whether the first character is not alpha.
-  if (s.empty())
-    throw std::runtime_error("Illegal string");
-  else if (!isalpha(s[0]))
-    throw std::runtime_error("Illegal string: " + s);
+    // Check whether string is empty or whether the first character is not alpha.
+    if (s.empty())
+        throw std::runtime_error("Illegal string");
+    else if (!isalpha(s[0]))
+        throw std::runtime_error("Illegal string: " + s);
 
-  // Return string if all characters are alphanumeric.
-  if (find_if(s.begin(), s.end(), [](const char c) { return !std::isalnum(c); }) == s.end())
-    return;
-  else
-    throw std::runtime_error("Illegal string: " + s);
+    // Return string if all characters are alphanumeric.
+    if (find_if(s.begin(), s.end(), [](const char c) { return !std::isalnum(c); }) == s.end())
+        return;
+    else
+        throw std::runtime_error("Illegal string: " + s);
 }
 
-void readIniFile(char *argv[])
+void readIniFile(const std::string &file_name)
 {
-  std::string blockname;
+    std::string blockname;
 
-  std::ifstream infile(argv[1]);
-  std::string line;
+    // Read file and throw exception on error.
+    std::ifstream infile;
+    infile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    infile.open(file_name);
 
-  while (std::getline(infile, line))
-  {
-    // Strip of the comments.
-    std::vector<std::string> strings;
-    boost::split(strings, line, boost::is_any_of("#"));
+    std::string line;
 
-    // Keep part that is not comment.
-    if (strings.size() >= 2)
-      line = strings[0];
-
-    // Strip of all the whitespace.
-    boost::trim(line);
-
-    // Split string on = character.
-    strings.clear();
-    boost::split(strings, line, boost::is_any_of("="));
-
-    // In case of one element, check whether we have a block header.
-    if (strings.size() == 1)
+    while (std::getline(infile, line))
     {
-      std::string header = strings[0];
-      boost::trim(header);
+        // Strip of the comments.
+        std::vector<std::string> strings;
+        boost::split(strings, line, boost::is_any_of("#"));
 
-      // If only an empty line remains, jump to the next line.
-      if (header.empty())
-        continue;
+        // Keep part that is not comment.
+        if (strings.size() >= 2)
+            line = strings[0];
 
-      // Store the block name and check for validity of string.
-      if (header.front() == '[' && header.back() == ']')
-      {
-        blockname = header.substr(1, header.size()-2);
-        checkItem(blockname);
-      }
-      else
-        throw std::runtime_error("Illegal line");
+        // Strip of all the whitespace.
+        boost::trim(line);
+
+        // Split string on = character.
+        strings.clear();
+        boost::split(strings, line, boost::is_any_of("="));
+
+        // In case of one element, check whether we have a block header.
+        if (strings.size() == 1)
+        {
+            std::string header = strings[0];
+            boost::trim(header);
+
+            // If only an empty line remains, jump to the next line.
+            if (header.empty())
+                continue;
+
+            // Store the block name and check for validity of string.
+            if (header.front() == '[' && header.back() == ']')
+            {
+                blockname = header.substr(1, header.size()-2);
+                checkItem(blockname);
+            }
+            else
+                throw std::runtime_error("Illegal line");
+        }
+        // Read item.
+        else if (strings.size() == 2)
+        {
+            if (blockname.empty())
+                throw std::runtime_error("No block name found");
+
+            std::string left = strings[0];
+            std::string right = strings[1];
+            boost::trim(left);
+            boost::trim(right);
+            checkItem(left);
+
+            // Leave the checking of the right string for later
+            // when the type is known.
+            itemlist[blockname][left] = right;
+        }
+        // Throw an error.
+        else
+            throw std::runtime_error("Illegal line");
     }
-    // Read item.
-    else if (strings.size() == 2)
-    {
-      if (blockname.empty())
-        throw std::runtime_error("No block name found");
 
-      std::string left = strings[0];
-      std::string right = strings[1];
-      boost::trim(left);
-      boost::trim(right);
-      checkItem(left);
-
-      // Leave the checking of the right string for later
-      // when the type is known.
-      itemlist[blockname][left] = right;
-    }
-    // Throw an error.
-    else
-      throw std::runtime_error("Illegal line");
-  }
-
-  // Print the list as a test.
-  for (auto &m : itemlist)
-    for (auto &s : m.second)
-      std::cout << m.first << "," << s.first << "," << s.second << ";" << std::endl;
+    // Print the list as a test.
+    for (auto &m : itemlist)
+        for (auto &s : m.second)
+            std::cout << m.first << "," << s.first << "," << s.second << ";" << std::endl;
 }
 
 std::string getItemString(const std::string &blockname, const std::string &itemname)
 {
-  auto itblock = itemlist.find(blockname);
-  if (itblock == itemlist.end())
-    throw std::runtime_error("Block does not exist");
+    auto itblock = itemlist.find(blockname);
+    if (itblock == itemlist.end())
+        throw std::runtime_error("Block does not exist");
 
-  auto ititem = itblock->second.find(itemname);
-  if (ititem == itblock->second.end())
-    throw std::runtime_error("Item does not exist");
+    auto ititem = itblock->second.find(itemname);
+    if (ititem == itblock->second.end())
+        throw std::runtime_error("Item does not exist");
 
-  return ititem->second;
+    return ititem->second;
 }
 
 template<typename T>
 T getItemFromStream(std::istringstream &ss)
 {
-  // Read the item from the stringstream, operator >> trims automatically.
-  T item;
-  if (!(ss >> item))
-    throw std::runtime_error("Item does not match type");
+    // Read the item from the stringstream, operator >> trims automatically.
+    T item;
+    if (!(ss >> item))
+        throw std::runtime_error("Item does not match type");
 
-  // Check whether stringstream is empty, if not type is incorrect.
-  std::string dummy;
-  if (ss >> dummy)
-    throw std::runtime_error("Partial item does not match type");
+    // Check whether stringstream is empty, if not type is incorrect.
+    std::string dummy;
+    if (ss >> dummy)
+        throw std::runtime_error("Partial item does not match type");
 
-  return item;
+    return item;
 }
 
 template<typename T>
 T getItem(const std::string &blockname, const std::string &itemname)
 {
-  std::string value = getItemString(blockname, itemname);
+    std::string value = getItemString(blockname, itemname);
 
-  std::istringstream ss(value);
+    std::istringstream ss(value);
 
-  T item = getItemFromStream<T>(ss);
-  checkItem<T>(item);
+    T item = getItemFromStream<T>(ss);
+    checkItem<T>(item);
 
-  return item;
+    return item;
 }
 
 template<typename T>
 std::vector<T> getList(const std::string &blockname, const std::string &itemname)
 {
-  std::string value = getItemString(blockname, itemname);
+    std::string value = getItemString(blockname, itemname);
 
-  std::vector<std::string> listitems;
-  boost::split(listitems, value, boost::is_any_of(","));
+    std::vector<std::string> listitems;
+    boost::split(listitems, value, boost::is_any_of(","));
 
-  std::vector<T> list;
-  for (std::string itemstring : listitems)
-  {
-    std::istringstream ss(itemstring);
-    T item = getItemFromStream<T>(ss);
-    checkItem(item);
-    list.push_back(item);
-  }
+    std::vector<T> list;
+    for (std::string itemstring : listitems)
+    {
+        std::istringstream ss(itemstring);
+        T item = getItemFromStream<T>(ss);
+        checkItem(item);
+        list.push_back(item);
+    }
 
-  return list;
+    return list;
 }
 
 int main(int argc, char *argv[])
 {
-  try
-  {
-    readIniFile(argv);
-    int itot = getItem<int>("grid", "itot");
-    double xsize = getItem<double>("grid", "xsize");
-    double zsize = getItem<double>("grid", "zsize");
-    std::string swthermo = getItem<std::string>("thermo", "swthermo");
-    std::vector<std::string> crosslist = getList<std::string>("cross", "crosslist");
-    std::vector<double> xy = getList<double>("cross", "xy");
+    try
+    {
+        if (argc != 2)
+            throw std::runtime_error("Illegal number or arguments");
 
-    std::cout << "itot = " << itot  << std::endl;
-    std::cout << "xsize = " << xsize << std::endl;
-    std::cout << "zsize = " << zsize << std::endl;
-    std::cout << "swthermo = " << swthermo << std::endl;
-    std::cout << "crosslist = ";
-    for (std::string &s : crosslist)
-      std::cout << "\"" << s << "\"" << " ";
-    std::cout << std::endl;
+        std::string file_name = std::string(argv[1]);
+        readIniFile(file_name);
 
-    std::cout << "xy = ";
-    for (double &i : xy)
-      std::cout << i << " ";
-    std::cout << std::endl;
-  }
-  catch (std::exception &e)
-  {
-    std::cout << "EXCEPTION: " << e.what() << std::endl;
-    return 1;
-  }
+        int itot = getItem<int>("grid", "itot");
+        double xsize = getItem<double>("grid", "xsize");
+        double zsize = getItem<double>("grid", "zsize");
+        std::string swthermo = getItem<std::string>("thermo", "swthermo");
+        std::vector<std::string> crosslist = getList<std::string>("cross", "crosslist");
+        std::vector<double> xy = getList<double>("cross", "xy");
 
-  return 0;
+        std::cout << "itot = " << itot  << std::endl;
+        std::cout << "xsize = " << xsize << std::endl;
+        std::cout << "zsize = " << zsize << std::endl;
+        std::cout << "swthermo = " << swthermo << std::endl;
+        std::cout << "crosslist = ";
+        for (std::string &s : crosslist)
+            std::cout << "\"" << s << "\"" << " ";
+        std::cout << std::endl;
+
+        std::cout << "xy = ";
+        for (double &i : xy)
+            std::cout << i << " ";
+        std::cout << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "EXCEPTION: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
