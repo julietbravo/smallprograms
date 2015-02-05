ef8b7215c03aa5784dc22350cc21f5e1f2999b58
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-02-05 23:50:42 +0100

    Crashing template on GCC, interesting case

---
+++ template_crash/crash.cxx
@@ -0,0 +1,95 @@
+struct Grid
+{
+  Grid(const int itot, const int gc) :
+    itot(itot), gc(gc), istart(gc), iend(itot+gc), icells(itot+2*gc) {}
+
+  const int itot;
+  const int gc;
+  const int istart;
+  const int iend;
+  const int icells;
+};
+
+template<int loc, class Inner>
+struct Interp
+{
+  Interp(const Inner& inner) : inner_(inner) {}
+
+  const Inner& inner_;
+
+  inline double operator()(const int i) const
+  {
+    return   (-1./16)*(inner_(i + (-2+loc)) + inner_(i + ( 1+loc)))
+           + ( 9./16)*(inner_(i + (-1+loc)) + inner_(i + (   loc)));
+  }
+};
+
+template<class Inner>
+inline Interp<1, Inner> Ix(const Inner& inner)
+{ return Interp<1, Inner>(inner); }
+
+template<class Inner>
+inline Interp<0, Inner> Ix_h(const Inner& inner)
+{ return Interp<0, Inner>(inner); }
+
+template<class Left, class Right>
+struct Multiply
+{
+  Multiply(const Left& left, const Right& right) : left_(left), right_(right) {}
+
+  const Left& left_;
+  const Right& right_;
+
+  inline double operator()(const int i) const
+  { return left_(i) * right_(i); }
+};
+
+template<class Left, class Right>
+inline Multiply<Left, Right> operator*(const Left& left, const Right& right)
+{
+  return Multiply<Left, Right>(left, right);
+}
+
+class Field
+{
+  public:
+    Field(const Grid& grid) :
+      grid_(grid),
+      data_(new double[grid_.icells]) {}
+
+    inline double operator()(const int i) const
+    { return data_[i]; }
+
+    inline double& operator()(const int i)
+    { return data_[i]; }
+
+    template<class T>
+    inline Field& operator+=(const T& expression)
+    {
+      for (int i=grid_.istart; i<grid_.iend; ++i)
+        (*this)(i) += expression(i);
+
+      return *this;
+    }
+
+  private:
+    const Grid& grid_;
+    double* data_;
+};
+
+int main()
+{
+  Grid grid(256, 4);
+
+  Field u (grid);
+  Field ut(grid);
+
+  // Like this it crashes
+  auto expression = Ix_h( Ix  (u) * Ix  (u) );
+  ut += expression;
+
+  // But like this it does not
+  ut += Ix_h( Ix  (u) * Ix  (u) );
+
+  return 0;
+}
