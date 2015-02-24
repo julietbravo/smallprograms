44a8af7d1edc55fed661b5af399fd0b8ce21a7ed
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-02-24 23:53:05 +0100

    Starting threadpool class

--- thread/thread.cxx
+++ thread/thread.cxx
@@ -5,18 +5,18 @@
 void printInt(const int n)
 { 
   std::cout << n << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  //std::this_thread::sleep_for(std::chrono::seconds(2));
 }
 
 void printHello()
 { 
   std::cout << "Hello!" << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  //std::this_thread::sleep_for(std::chrono::seconds(2));
 }
 
 int calcSquare(const int n)
 { 
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  //std::this_thread::sleep_for(std::chrono::seconds(2));
   return n*n;
 }
 
@@ -25,13 +25,41 @@ auto doWork(F& f, Args... args) -> decltype( f(args...) )
 {
   std::packaged_task<decltype( f(args...) )(Args...)> task(&f);
   auto fut = task.get_future();
-  std::thread t(std::move(task), args...);
-  t.join();
+  std::thread tloc(std::move(task), args...);
+  tloc.join();
   return fut.get();
 }
 
+
+class ThreadPool
+{
+  public:
+    ThreadPool() : active_(true)
+    {
+      t_ = std::thread([&]()
+          { 
+            while (active_)
+            {
+            }
+          });
+    }
+
+    ~ThreadPool()
+    {
+      std::cout << "Terminating thread pool!" << std::endl;
+      active_ = false;
+      t_.join();
+    }
+
+  private:
+    std::atomic<bool> active_;
+    std::thread t_;
+};
+
 int main()
 {
+  ThreadPool threadPool;
+
   doWork(printInt, 10);
   doWork(printHello);
   auto c = doWork(calcSquare, 4);
