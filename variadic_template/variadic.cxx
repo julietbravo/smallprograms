c8e5fe3964675f124fd5adcfb7c65706da21f312
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-02-24 12:04:33 +0100

    Cleanup

--- variadic_template/variadic.cxx
+++ variadic_template/variadic.cxx
@@ -1,14 +1,7 @@
 #include <iostream>
 
-void printInt(const int i)
-{
-  std::cout << i << std::endl;
-}
-
-void printHello()
-{
-  std::cout << "Hello" << std::endl;
-}
+void printInt(const int i) { std::cout << i << std::endl; }
+void printHello() { std::cout << "Hello!" << std::endl; }
 
 template<class F, class... Args>
 void doWork(F f, Args... args)
@@ -18,7 +11,6 @@ void doWork(F f, Args... args)
 
 int main()
 {
-  const int i = 10;
   doWork(printInt, 10);
   doWork(printHello);
 
