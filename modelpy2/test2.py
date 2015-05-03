8aa8adf37f8b9c50729a697cc334459527c39e36
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-05-03 18:11:55 +0200

    add another simple experiment

--- modelpy2/test2.py
+++ modelpy2/test2.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pylab as pl
+
 from atmosphere import AtmosphereConstant
 from surface import SurfaceFixedTemperature
 
@@ -10,3 +13,16 @@ atmosphere_simple = AtmosphereConstant(atmosphere_input_simple)
 
 surface_simple.state(atmosphere_simple)
 print("Calculated wtheta = {0}".format(surface_simple.wtheta))
+
+ra_array = np.linspace(10., 50, 100)
+wtheta_array = np.empty(ra_array.size)
+
+for n in range(ra_array.size):
+    surface_simple.ra = ra_array[n]
+    surface_simple.state(atmosphere_simple)
+    wtheta_array[n] = surface_simple.wtheta
+
+pl.figure()
+pl.plot(ra_array, wtheta_array)
+pl.xlabel('ra')
+pl.ylabel('wtheta')
